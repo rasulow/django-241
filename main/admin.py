2815e60a3e7c18126e90cbf649ededcd1cec5ace
unknown <Administrator@IT-MUGALLYM.int.local>
2024-03-12 12:35:12 +0500

    product table, create button, delete, update

--- main/admin.py
+++ main/admin.py
@@ -2,5 +2,4 @@ from django.contrib import admin
 from . import models
 
 
-admin.site.register(models.Category)
 admin.site.register(models.Product)
--- main/models.py
+++ main/models.py
@@ -1,23 +1,9 @@
-from django.db import models
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=255)
-    
-    class Meta:
-        db_table='category'
-        verbose_name = 'Category'
-        verbose_name_plural = 'Categories'
-        
-    def __str__(self) -> str:
-        return self.name
-    
+from django.db import models    
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.FloatField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     
     class Meta:
         db_table='Product'
