74db336ff4890b7e101c022a6165a7150c12f30b
unknown <Administrator@IT-MUGALLYM.int.local>
2024-03-12 13:10:15 +0500

    commit

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('create/', views.create_product, name='create'),
+    path('delete/<int:id>/', views.delete_product, name='delete'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import models
 
 def home(request):
@@ -7,3 +7,19 @@ def home(request):
         'products': products
     }
     return render(request, 'home.html', context)
+
+
+def create_product(request):
+    if request.method == 'POST':
+        name = request.POST.get('name', '')
+        description = request.POST.get('description', '')
+        price = request.POST.get('price', 0.0)
+        models.Product.objects.create(name=name, description=description, price=price)
+        return redirect('home')
+    return render(request, 'create.html')
+
+
+def delete_product(request, id):
+    product = models.Product.objects.get(id=id)
+    product.delete()
+    return redirect('home')
